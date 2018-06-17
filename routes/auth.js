9e479e69abbd5b42a507297c82a7a3b1226c3748
Eric Murphy <ericnmurphy@gmail.com>
2018-06-17 14:31:05 +0700

    Added .env variables

---
+++ routes/auth.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+router.get('/google', passport.authenticate('google', {scope: ['profile']}));
+
+router.get('/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
+    //successful authentication, redirect to dashboard
+    res.redirect('/dashboard');
+  }
+);
+
+module.exports = router;
