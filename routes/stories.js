0e2e0644b371a270b8227e1be1764c2be44a6b67
Eric Murphy <ericnmurphy@gmail.com>
2018-06-19 21:00:11 +0700

    Add index for public stories

--- routes/stories.js
+++ routes/stories.js
@@ -5,8 +5,16 @@ const Story = mongoose.model('stories');
 const User = mongoose.model('users');
 const {ensureAuthenticated} = require('../helpers/auth');
 
-router.get('/', ensureAuthenticated, (req, res) => {
-  res.render('stories/index');
+
+//stories index
+router.get('/', (req, res) => {
+  Story.find({status: 'public'})
+    .populate('user')
+    .then(stories => {
+      res.render('stories/index', {
+        stories: stories
+      });
+    });
 });
 
 router.get('/add', ensureAuthenticated, (req, res) => {
