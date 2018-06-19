db8a4d9fe728c9f0f348e666fd6fd93901b681db
Eric Murphy <ericnmurphy@gmail.com>
2018-06-19 10:19:59 +0700

    Added authentication to pages

--- routes/stories.js
+++ routes/stories.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const {ensureAuthenticated} = require('../helpers/auth');
 
-router.get('/', (req, res) => {
+router.get('/', ensureAuthenticated, (req, res) => {
   res.render('stories/index');
 });
 
